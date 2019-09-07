6c6093b73e4dab3552f3563e0ffcb3a11561b291
RodrigoOBC <rodrigooliveiracabral@yahoo.com.br>
2019-09-07 17:48:51 -0300

    Primeiro Commit do banco de dados

--- Classes/Banco_de_Dados.py
+++ Classes/Banco_de_Dados.py
@@ -1,6 +1,50 @@
+import psycopg2
+from postgres import Postgres
 
 
+class Conectar:
+    def __init__(self, host, DB, user, password):
+        self.host = host
+        self.db = DB
+        self.user = user
+        self.password = password
+        self.con = None
+        self.valores = None
 
+    def Conectar(self) -> bool:
+        try:
+            self.con = Postgres(f"postgres://{self.user}:{self.password}@{self.host}:5432/{self.db}")
+        except:
+            return False
+        finally:
+            return True
+
+    def select(self,query):
+        try:
+            self.Conectar()
+            self.valores = self.con.all(query)
+        except:
+            return False
+        finally:
+            return self.valores
+
+    def update(self,query) -> bool:
+        try:
+            self.Conectar()
+            self.con.run(query)
+        except:
+            return False
+        finally:
+            return True
+
+    def insert(self,query):
+        try:
+            self.Conectar()
+            self.con.run(query)
+        except:
+            return False
+        finally:
+            return True
 
 
 if __name__ == '__main__':
--- models/Fuzzy_exemplo.py
+++ models/Fuzzy_exemplo.py
@@ -2,24 +2,22 @@ import numpy as np
 import skfuzzy as fuzz
 
 
-def entrada():
-    exercicio = 2
-    taxa_morte_municipio = 199
-    cancer_historico = 1
-    exercicio = int((exercicio * 300) / 7)
-
-
-def calculos(exercicio, taxa_morte_municipio, cancer_historico):
-    x_exercicios = np.arange(0, 301, 1)  # execicios por dia [0,7]
+def calculos(exercicio, taxa_morte_municipio, cancer_risco, idade, imc, idade_sexo, frequencia_hospitalar):
+    x_exercicios = np.arange(0, 300, 1)  # execicios por dia [0,7]
     x_taxa_morte_municipio = np.arange(0, 301, 1)  # taxa de homicidios por ano [0,300]
-    x_cancer_historico = np.arange(0, 2, 1)  # historico de cancer 0 para ter cancer, 1 para não ter cancer  [0,300]
+    x_cancer_risco = np.arange(0, 101, 1)  # historico de cancer 0 para ter cancer, 1 para não ter cancer  [0,300]
     x_saida = np.arange(0, 101, 1)  # saida em porcento
 
     print(x_saida)
 
-    exercicio_baixo = fuzz.trimf(x_exercicios, [0, 64, 128])
-    exercicio_normal = fuzz.trimf(x_exercicios, [85, 150, 214])
-    exercicio_alto = fuzz.trimf(x_exercicios, [171, 235, 300])
+    if 18 <= idade <= 64:
+        exercicio_baixo = fuzz.trimf(x_exercicios, [0, 20, 30])
+        exercicio_normal = fuzz.trimf(x_exercicios, [28, 50, 60])
+        exercicio_alto = fuzz.trimf(x_exercicios, [58, 60, 300])
+    elif idade < 18:
+        exercicio_baixo = fuzz.trimf(x_exercicios, [0, 50, 75])
+        exercicio_normal = fuzz.trimf(x_exercicios, [75, 150, 214])
+        exercicio_alto = fuzz.trimf(x_exercicios, [149, 235, 300])
 
     # municipio risco
 
@@ -29,60 +27,76 @@ def calculos(exercicio, taxa_morte_municipio, cancer_historico):
     taxa_morte_municipio_alto = fuzz.trimf(x_taxa_morte_municipio, [60, 70, 80])
     taxa_morte_municipio_altissimo = fuzz.trimf(x_taxa_morte_municipio, [80, 100, 200])
 
+    # imc
+
     # cancer risco
 
-    cancer_baixo = fuzz.trimf(x_cancer_historico, [0, 0, 1])
-    cancer_alto = fuzz.trimf(x_cancer_historico, [1, 1, 1])
+    ''' cancer ficará para a proxima implemntação'''
+
+    cancer_baixo = fuzz.trimf(x_cancer_risco, [0, 30, 34])
+    cancer_medio = fuzz.trimf(x_cancer_risco, [32, 50, 60])
+    cancer_alto = fuzz.trimf(x_cancer_risco, [58, 70, 100])
+
+
+
 
     # saida
 
-    perigo_baixissimo_x = fuzz.trimf(x_saida, [0, 10, 20])
+    perigo_muito_baixo_x = fuzz.trimf(x_saida, [0, 10, 20])
     perigo_baixo_x = fuzz.trimf(x_saida, [15, 30, 40])
     perigo_medio_x = fuzz.trimf(x_saida, [40, 50, 60])
     perigo_alto_x = fuzz.trimf(x_saida, [60, 70, 80])
-    perigo_muito_alto_x = fuzz.trimf(x_saida, [80, 90, 100])
+    perigo_de_risco_x = fuzz.trimf(x_saida, [80, 90, 100])
 
-    exercicio_level_baixo = fuzz.interp_membership(x_exercicios, exercicio_baixo, exercicio)
-    exercicio_level_normal = fuzz.interp_membership(x_exercicios, exercicio_normal, exercicio)
-    exercicio_level_alto = fuzz.interp_membership(x_exercicios, exercicio_alto, exercicio)
+    # # Definindo os leveis de cada perigo
+    # exercicio_level_baixo = fuzz.interp_membership(x_exercicios, exercicio_baixo, exercicio)
+    # exercicio_level_normal = fuzz.interp_membership(x_exercicios, exercicio_normal, exercicio)
+    # exercicio_level_alto = fuzz.interp_membership(x_exercicios, exercicio_alto, exercicio)
+    #
+    # # fazendo a vendo qual a relação dos municipios e classificando
+    # taxa_morte_municipio_level_muito_baixo_ = fuzz.interp_membership(x_taxa_morte_municipio,
+    #                                                                  taxa_morte_municipio_baixissimo,
+    #                                                                  taxa_morte_municipio)
+    # taxa_morte_municipio_level_baixo = fuzz.interp_membership(x_taxa_morte_municipio, taxa_morte_municipio_baixo,
+    #                                                           taxa_morte_municipio)
+    # taxa_morte_municipio_level_normal = fuzz.interp_membership(x_taxa_morte_municipio, taxa_morte_municipio_normal,
+    #                                                            taxa_morte_municipio)
+    # taxa_morte_municipio_level_alto = fuzz.interp_membership(x_taxa_morte_municipio, taxa_morte_municipio_alto,
+    #                                                          taxa_morte_municipio)
+    # taxa_morte_municipio_level_de_risco = fuzz.interp_membership(x_taxa_morte_municipio,
+    #                                                              taxa_morte_municipio_altissimo,
+    #                                                              taxa_morte_municipio)
+    #
+    # # vendo a realação do cancer
+    # cancer_historico_level_baixo = fuzz.interp_membership(x_cancer_risco, cancer_baixo, cancer_risco)
+    #
+    # cancer_historico_level_medio = fuzz.interp_membership(x_cancer_risco, cancer_medio, cancer_risco)
+    #
+    # cancer_historico_level_alto = fuzz.interp_membership(x_cancer_risco, cancer_medio, cancer_risco)
+    #
+    # # perigo altissimo
+    #
+    # '''
+    #     perigo_de_risco_x
+    #     ou
+    #     taxa_morte_municipio_level_de_risco e (cancer_historico_level_Sim e exercicio_level_baixo)
+    #     ou
+    #     taxa_morte_municipio_level_de_risco e (cancer_historico_level_Sim,exercicio_level_normal)
+    # '''
+    #
+    # perigo_altissimo = np.fmin(
+    #     np.fmin(
+    #         np.fmax(taxa_morte_municipio_level_de_risco,
+    #                 np.fmax(cancer_historico_level_Sim, exercicio_level_baixo)),
+    #         np.fmax(taxa_morte_municipio_level_de_risco,
+    #                 np.fmax(cancer_historico_level_Sim, exercicio_level_normal))),
+    #     perigo_de_risco_x)
+    #
+    # '''
+    #
+    #
+    # '''
 
-    # fazendo a vendo qual a relação dos municipios e classificando
-    taxa_morte_municipio_level_baixissimo = fuzz.interp_membership(x_taxa_morte_municipio,
-                                                                   taxa_morte_municipio_baixissimo,
-                                                                   taxa_morte_municipio)
-    taxa_morte_municipio_level_baixo = fuzz.interp_membership(x_taxa_morte_municipio, taxa_morte_municipio_baixo,
-                                                              taxa_morte_municipio)
-    taxa_morte_municipio_level_normal = fuzz.interp_membership(x_taxa_morte_municipio, taxa_morte_municipio_normal,
-                                                               taxa_morte_municipio)
-    taxa_morte_municipio_level_alto = fuzz.interp_membership(x_taxa_morte_municipio, taxa_morte_municipio_alto,
-                                                             taxa_morte_municipio)
-    taxa_morte_municipio_level_altissimo = fuzz.interp_membership(x_taxa_morte_municipio,
-                                                                  taxa_morte_municipio_altissimo,
-                                                                  taxa_morte_municipio)
 
-    # vendo a realação do cancer
-    cancer_historico_level_Nao = fuzz.interp_membership(x_cancer_historico, cancer_baixo, cancer_historico)
-    cancer_historico_level_Sim = fuzz.interp_membership(x_cancer_historico, cancer_alto, cancer_historico)
-
-    # perigo altissimo
-
-    '''
-        perigo_muito_alto_x 
-        ou 
-        taxa_morte_municipio_level_altissimo e (cancer_historico_level_Sim e exercicio_level_baixo)
-        ou
-        taxa_morte_municipio_level_altissimo e (cancer_historico_level_Sim,exercicio_level_normal)
-    '''
-
-    perigo_altissimo = np.fmin(
-                        np.fmin(
-                                np.fmax(taxa_morte_municipio_level_altissimo,
-                                        np.fmax(cancer_historico_level_Sim,exercicio_level_baixo)),
-                                        np.fmax(taxa_morte_municipio_level_altissimo,
-                                                np.fmax(cancer_historico_level_Sim,exercicio_level_normal))),
-                                perigo_muito_alto_x)
-
-    '''
-    
-    
-    '''
+if __name__ == '__main__':
+    pass
